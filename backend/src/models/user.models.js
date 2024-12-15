26c9610026033a860848c3f8541b22461f94f8c0
Luisa <luisacoutinho06@gmail.com>
2024-12-15 11:17:48 -0300

    Inserindo os métodos referentes ao usuário.

---
+++ backend/src/models/user.models.js
@@ -0,0 +1,69 @@
+// DESCRIÇÃO: Arquivo responsável pelo modelo da classe 'User'
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: { type: String, maxlength: 50, required: true },
+    email: { type: String, maxlength: 30, required: true },
+    password: { type: String, required: true },
+    tokens: [
+      {
+        token: { type: String, require: true },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    collection: "users",
+  }
+);
+
+// ==> Este método será responsável por fazer o Hash da senha antes de salvar a classe do modelo User.
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+// ==> Este método será responsável por gerar uma autenticação Auth, para o User.
+userSchema.methods.generateAuthToken = async function (params) {
+  const user = this;
+  const token = jwt.sign(
+    { _id: user._id, name: user.name, email: user.email },
+    "secret"
+  );
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+// ==> Este método será responsável por fazer uma pesquisar do user por email, ou o password.
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  console.log(user);
+
+  if (!user) {
+    throw new Error({ error: "Login inválido!" });
+  }
+
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordMatch) {
+    throw new Error({ error: "Senha inválida!" });
+  }
+
+  return user;
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
